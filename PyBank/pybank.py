5c8bda44cb160771dfa7ef6bf415ec1498909bc1
Hans Engelbrecht <hengelbrecht@godaddy.com>
2020-03-11 00:45:48 -0700

    added output to text file

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -2,14 +2,16 @@
 import os
 import csv
 
+#set variable for csv path
 pybank_csv = os.path.join('..', 'Resources', 'budget_data.csv')
 
-   
+#Read CSV   
 with open(pybank_csv, 'r') as csvfile:
     #split data on commas
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
 
+    #Declare variables to be used in for loop
     row_count = 0
     pnl = 0
     max_pnl = 0
@@ -42,6 +44,17 @@ with open(pybank_csv, 'r') as csvfile:
     print(f"There are {row_count} months")     
     print(f"The net profit/loss is ${pnl}") 
     print(f"The average profit/loss is ${round(ave_pnl)}") 
-    print(f"The largest profit increase is: {max_date}  ${max_pnl}")
+    print(f"The largest profit increase is: {max_date} ${max_pnl}")
     print(f"The largest profit decrease is: {min_date} ${min_pnl}")
     print("------------------------------------------------------") 
+
+    with open("Output.txt", "w") as text_file:
+        print("------------------------------------------------------", file=text_file)
+        print("              Financial Analysis", file=text_file)
+        print("------------------------------------------------------", file=text_file)
+        print(f"There are {row_count} months", file=text_file)     
+        print(f"The net profit/loss is ${pnl}", file=text_file) 
+        print(f"The average profit/loss is ${round(ave_pnl)}", file=text_file) 
+        print(f"The largest profit increase is: {max_date} ${max_pnl}", file=text_file)
+        print(f"The largest profit decrease is: {min_date} ${min_pnl}", file=text_file)
+        print("------------------------------------------------------", file=text_file) 
